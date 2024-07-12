87a50869dd0bcbb8570021ed71fe8a9062086041
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-07-12 15:28:06 -0700

    Implement SignIn and SignUp

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
@@ -6,6 +6,7 @@ import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 import { Link } from 'expo-router'
+import { signIn } from '../../lib/appwrite'
 
 
 const SignIn = () => {
@@ -16,8 +17,25 @@ const SignIn = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = () => {
-
+  const submit = async () => {
+    if(!form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields');
+      return;
+    }
+  
+    setIsSubmitting(true);
+  
+    try {
+      await signIn(form.email, form.password);
+  
+      // Set to global state ......
+  
+      router.replace('/home');
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
   }
 
   return (
@@ -48,7 +66,7 @@ const SignIn = () => {
           />
 
           <CustomButton 
-            title="Sign-in"
+            title="Sign In"
             handlePress={submit}
             containerStyles="mt-7"
             isLoading={isSubmitting}
